00ded0755203e1b8629b74dacf90f5e769aecd15
SnomFish <arthurbaverstock21@gmail.com>
2026-04-15 21:39:12 +0100

    added backend

---
+++ backend/src/main/java/io/github/snomfish/SnomfishApplication.java
@@ -0,0 +1,13 @@
+package io.github.snomfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnomfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnomfishApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/snomfish/SnomfishApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.snomfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnomfishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
